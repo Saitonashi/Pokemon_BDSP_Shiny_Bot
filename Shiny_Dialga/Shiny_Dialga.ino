e5ab0efff32dbd8842d685bd87efd11bed3534b7
Kamil Terka <Saitonashi@gmail.com>
2021-12-15 20:03:49 +0100

    Update TCS34725 sensor

--- Shiny_Dialga/Shiny_Dialga.ino
+++ Shiny_Dialga/Shiny_Dialga.ino
@@ -1,5 +1,5 @@
 //Servo pins: 6(HOME), 9(A), 10(X), 11(UP)
-//LED pin: 8
+//LED pin: 4
 //Color Sensor pin: A4(SDA), A5(SCL)
 
 #include <Wire.h>
@@ -10,33 +10,66 @@ Servo HOME_Servo;
 Servo A_Servo;
 Servo X_Servo;
 Servo Up_Servo;
-int max_pos = 30;
-int min_pos = 0;
 int pos;
 
 /* Connect SCL    to analog 5
    Connect SDA    to analog 4
-   Connect VDD    to 3.3V DC
+   Connect VDD    to 5V DC
    Connect GROUND to common ground */
-Adafruit_TCS34725 Color_Sensor = Adafruit_TCS34725();
-uint16_t red, green, blue, c; //, colorTemp, lux;
+Adafruit_TCS34725 Color_Sensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+uint16_t red, green, blue, c;   // colorTemp, lux;
 
 int resets = 0;
 bool shiny = false;
 
-//PRESS BUTTON
-//press_button(HOME_Servo);
-//press_button(A_Servo);
-//press_button(X_Servo);
-//press_button(Up_Servo);
-void press_button(Servo servo) {
-  for (pos = min_pos; pos <= max_pos; pos++) {
-    servo.write(pos);
-    delay(15);
+//PRESS HOME BUTTON
+void press_Home_button() {
+  for (pos = 90; pos <= 142; pos += 1) {
+    HOME_Servo.write(pos);
+    delay(5);
   }
-  for (pos = max_pos; pos >= min_pos; pos--) {
-    servo.write(pos);
-    delay(15);
+  delay(100);
+  for (pos = 142; pos >= 90; pos -= 1) {
+    HOME_Servo.write(pos);
+    delay(5);
+  }
+}
+
+//PRESS A BUTTON
+void press_A_button() {
+  for (pos = 90; pos >= 40; pos -= 1) {
+    A_Servo.write(pos);
+    delay(5);
+  }
+  for (pos = 40 ; pos <= 90; pos += 1) {
+    A_Servo.write(pos);
+    delay(5);
+  }
+}
+
+//PRESS X BUTTON
+void press_X_button() {
+  for (pos = 90; pos >= 40; pos -= 1) {
+    X_Servo.write(pos);
+    delay(5);
+  }
+  for (pos = 40; pos <= 90; pos += 1) {
+    X_Servo.write(pos);
+    delay(5);
+  }
+}
+
+//PRESS UP BUTTON
+void press_Up_button() {
+  for (pos = 90; pos >= 40; pos -= 1) {
+    Up_Servo.write(pos);
+    delay(5);
+  }
+  delay(250
+       );
+  for (pos = 40; pos <= 90; pos += 1) {
+    Up_Servo.write(pos);
+    delay(5);
   }
 }
 
@@ -48,16 +81,21 @@ void setup() {
   A_Servo.attach(9);
   X_Servo.attach(10);
   Up_Servo.attach(11);
+  //Init LED
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
 
   //Reset servos position
-  HOME_Servo.write(0);
-  delay(100);
-  A_Servo.write(0);
-  delay(100);
-  X_Servo.write(0);
-  delay(100);
-  Up_Servo.write(0);
-  delay(100);
+  Serial.println("Reset servos position");
+  delay(4000);
+  HOME_Servo.write(90);
+  delay(500);
+  A_Servo.write(90);
+  delay(500);
+  X_Servo.write(90);
+  delay(500);
+  Up_Servo.write(90);
+  delay(500);
 
   //Check color sensor connection
   if (Color_Sensor.begin()) {
@@ -66,48 +104,53 @@ void setup() {
     Serial.println("No TCS34725 found ... check your connections");
     while (1);
   }
+  Color_Sensor.setInterrupt(true);
 
-  //Init LED
-  pinMode(8,OUTPUT);
-  digitalWrite(8, LOW);
+  digitalWrite(4, LOW);
 }
 
 void loop() {
   while (!shiny) {
     //Show reset count
-    Serial.println("Reset " + resets);
+    Serial.print("Reset ");
+    Serial.print(resets);
+    Serial.print(" ; ");
 
     //Lauch game
-    press_button(A_Servo);
+    press_A_button();
     delay(1500);
-    press_button(A_Servo);
+    press_A_button();
 
     //Wait for load game
     delay(25000);
-    press_button(A_Servo);
+    press_A_button();
     delay(3000);
-    press_button(A_Servo);
+    press_A_button();
     delay(13000);
 
     //Interact with Dialga
-    press_button(Up_Servo);
+    press_Up_button();
     delay(3000);
-    press_button(A_Servo);
+    press_A_button();
     delay(1000);
-    press_button(A_Servo);
-    delay(4000);
+    press_A_button();
+    delay(4150);
 
     //Check Dialga color
     Color_Sensor.getRawData(&red, &green, &blue, &c);
-    Serial.print("R: " + red);
-    Serial.print(" G: " + green);
-    Serial.println(" B: " + blue);
+    Serial.print("R: ");
+    Serial.print(red, DEC);
+    Serial.print(" G: ");
+    Serial.print(green, DEC);
+    Serial.print(" B: ");
+    Serial.print(blue, DEC);
+    Serial.print(" ; ");
 
     //If Diagla is not blue, is shiny
-    if (blue < 100) {
+    if (blue < 600) {
       shiny = true;
       Serial.println("SHINY!!!");
-      digitalWrite(8, HIGH);    //Light up LED to communicate shiny pokemon shows up
+      digitalWrite(4, HIGH);    //Light up LED to communicate shiny pokemon shows up
       continue;
     } else {
       Serial.println("Non Shiny :<");
@@ -115,11 +158,11 @@ void loop() {
 
     //Exit game
     if (!shiny) {   //condition for a security against resetting the game if shiny shows up
-      press_button(HOME_Servo);
+      press_Home_button();
       delay(1000);
-      press_button(X_Servo);
+      press_X_button();
       delay(1000);
-      press_button(A_Servo);
+      press_A_button();
       delay(1000);
       resets++;
     }
